6bbdd424fdd0f1d7177cbcbb2eb4613c469174c8
Sidney Andrews <sidandrews@microsoft.com>
2024-11-14 13:10:33 -0500

    Implement remainder of CRUD

--- src/app/main.rs
+++ src/app/main.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use futures::StreamExt;
 
 use dotenv;
 
@@ -42,34 +43,132 @@ async fn main() {
 
     let container_client = database_client.container_client(&container_name);
 
-    let item = Item {
-        id: "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb".to_string(),
-        category: "gear-surf-surfboards".to_string(),
-        name: "Yamba Surfboard".to_string(),
-        quantity: 12,
-        price: 850.00,
-        clearance: false,
-    };
+    {
+        let item = Item {
+            id: "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb".to_string(),
+            category: "gear-surf-surfboards".to_string(),
+            name: "Yamba Surfboard".to_string(),
+            quantity: 12,
+            price: 850.00,
+            clearance: false,
+        };
 
-    let partition_key = PartitionKey::from(item.category.clone());
+        let partition_key = PartitionKey::from(item.category.clone());
 
-    let upsert_response = container_client.upsert_item(partition_key, item, None).await;
-    match upsert_response {
-        Ok(i) => {
-            println!("Item upserted successfully.");
-            let deserialize_response = i.deserialize_body().await;
-            match deserialize_response {
-                Ok(i) => {
-                    let upserted_item = i.unwrap();
-                    println!("Upserted item: {:?}", upserted_item);
-                },
-                Err(e) => {
-                    eprintln!("Error deserializing response: {}", e);
-                },
-            }
-        },
-        Err(e) => {
-            eprintln!("Error upserting item: {}", e);
-        },
+        let upsert_response = container_client.upsert_item(partition_key, item, None).await;
+
+        match upsert_response {
+            Ok(r) => {
+                let deserialize_response = r.deserialize_body().await;
+                match deserialize_response {
+                    Ok(i) => {
+                        let upserted_item = i.unwrap();
+                        println!("Upserted item: {:?}", upserted_item);
+                    },
+                    Err(e) => {
+                        eprintln!("Error deserializing response: {}", e);
+                    },
+                }
+            },
+            Err(e) => {
+                eprintln!("Error upserting item: {}", e);
+            },
+        }
+    }
+    
+    {
+        let item = Item {
+            id: "bbbbbbbb-1111-2222-3333-cccccccccccc".to_string(),
+            category: "gear-surf-surfboards".to_string(),
+            name: "Kiama Classic Surfboard".to_string(),
+            quantity: 25,
+            price: 790.00,
+            clearance: true,
+        };
+
+        let partition_key = PartitionKey::from(item.category.clone());
+
+        let upsert_response = container_client.upsert_item(partition_key, item, None).await;
+
+        match upsert_response {
+            Ok(r) => {
+                let deserialize_response = r.deserialize_body().await;
+                match deserialize_response {
+                    Ok(i) => {
+                        let upserted_item = i.unwrap();
+                        println!("Upserted item: {:?}", upserted_item);
+                    },
+                    Err(e) => {
+                        eprintln!("Error deserializing response: {}", e);
+                    },
+                }
+            },
+            Err(e) => {
+                eprintln!("Error upserting item: {}", e);
+            },
+        }
+    }
+
+    {
+        let item_id = "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb";
+        let item_partition_key = "gear-surf-surfboards";
+
+        let read_response = container_client.read_item::<Item>(item_partition_key, item_id, None).await;
+
+        match read_response {
+            Ok(r) => {
+                let deserialize_response = r.deserialize_body().await;
+                match deserialize_response {
+                    Ok(i) => {
+                        let read_item = i.unwrap();
+                        println!("Read item: {:?}", read_item);
+                    },
+                    Err(e) => {
+                        eprintln!("Error deserializing response: {}", e);
+                    },
+                }
+            },
+            Err(e) => {
+                eprintln!("Error reading item: {}", e);
+            },
+        }
+    }
+
+    {
+        let item_partition_key = "gear-surf-surfboards";
+
+        let partition_key = PartitionKey::from(item_partition_key);
+
+        let query = format!("SELECT * FROM c WHERE c.category = '{}'", item_partition_key);
+
+        let page_response = container_client.query_items::<Item>(&query, partition_key, None);
+
+        match page_response {
+            Ok(mut page) => {
+                while let Some(item) = page.next().await {
+                    match item {
+                        Ok(i) => {
+                            let deserialize_response = i.deserialize_body().await;
+                            match deserialize_response {
+                                Ok(page) => {
+                                    for item in page.items {
+                                        println!("Queried item: {:?}", item);
+                                    }
+                                },
+                                Err(e) => {
+                                    eprintln!("Error deserializing item: {}", e);
+                                },
+                            }
+                        },
+                        Err(e) => {
+                            eprintln!("Error querying item: {}", e);
+                        },
+                    }
+                }
+            },
+            Err(e) => {
+                eprintln!("Error querying items: {}", e);
+            },
+        }
     }
 }
