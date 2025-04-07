165040a8ec02614c1511f41b231ced17bcf836d6
Sidney Andrews <sidandrews@microsoft.com>
2025-04-07 15:49:55 -0400

    Revert to `DefaultAzureCredential`

--- src/app/cosmos.rs
+++ src/app/cosmos.rs
@@ -1,6 +1,6 @@
 use serde_json;
 use azure_data_cosmos::{CosmosClient, PartitionKey, Query};
-use azure_identity::ManagedIdentityCredential;
+use azure_identity::DefaultAzureCredential;
 use futures::stream::StreamExt;
 use crate::item::Item;
 
@@ -16,7 +16,7 @@ where
     callback("Current Status:\tStarting...".to_string());
 
     // <create_client>
-    let credential = ManagedIdentityCredential::new()?;
+    let credential = DefaultAzureCredential::new()?;
 
     let client = CosmosClient::new(&endpoint, credential, None)?;
     // </create_client>
